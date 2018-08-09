9ee1c8f301b9ef2ce023ef8d80c1ddd03b716aea
Yuhsuke Gozu <yukkenottuna@gmail.com>
2018-08-09 22:02:23 +0900

    3を渡したら文字列"Fizz"を返す Refactor

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,7 @@
 class FizzBuzz
 
   def convert(number)
-    return "Fizz" if (number == 3)
+    return "Fizz" if (number % 3 == 0)
     number.to_s
   end
 
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -2,21 +2,17 @@ require 'spec_helper'
 
 RSpec.describe 'Fizz Buzz' do
 
-  it '1を渡したら文字列"1"を返す' do
-    fizz_buzz = FizzBuzz.new
+  let(:fizz_buzz) { FizzBuzz.new }
 
+  it '1を渡したら文字列"1"を返す' do
     expect(fizz_buzz.convert(1)).to eq "1"
   end
 
   it '2を渡したら文字列"2"を返す' do
-    fizz_buzz = FizzBuzz.new
-
     expect(fizz_buzz.convert(2)).to eq "2"
   end
 
   it '3を渡したら文字列"Fizz"を返す' do
-    fizz_buzz = FizzBuzz.new
-
     expect(fizz_buzz.convert(3)).to eq "Fizz"
   end
 
