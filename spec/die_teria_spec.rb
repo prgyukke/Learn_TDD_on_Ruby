f212fe8507120f5e3dafb85ff8ef54ac59360b39
Yuhsuke Gozu <yukkenottuna@gmail.com>
2018-08-10 00:09:34 +0900

    うるう日の場合、年を越す場合のテストを追加 GREEN

--- spec/die_teria_spec.rb
+++ spec/die_teria_spec.rb
@@ -7,10 +7,19 @@ RSpec.describe 'Die teria' do
       die_teria = DieTeria.new
       expect(die_teria.convert("2000/01/01")).to eq "2000/01/03"
     end
+  end
 
-    it "2000/01/02を渡したら2000/01/04を返す" do
+  describe "うるう日の場合" do
+    it "2000/02/27を渡したら2000/02/29を返す" do
       die_teria = DieTeria.new
-      expect(die_teria.convert("2000/01/02")).to eq "2000/01/04"
+      expect(die_teria.convert("2000/02/27")).to eq "2000/02/29"
+    end
+  end
+
+  describe "年を越す場合" do
+    it "2016/12/30を渡したら2017/01/01を返す" do
+      die_teria = DieTeria.new
+      expect(die_teria.convert("2016/12/30")).to eq "2017/01/01"
     end
   end
 end
